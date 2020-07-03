70d696ce7f8eda736627272703425e56ec493a1f
mikenspired <mikenspired@gmail.com>
2020-07-02 22:40:36 -0700

    Playermovement Working

--- Assets/PlayerRigMovement.cs
+++ Assets/PlayerRigMovement.cs
@@ -5,23 +5,25 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class PlayerRigMovement : MonoBehaviour
 {
-    public List<XRNode> xrNodes;
-    InputAxes TurningInput = InputAxes.Primary2DAxis;
-    public List<XRController> controllers;
+    public XRNode controllerType;
+    public InputAxes TurningInput = InputAxes.Primary2DAxis;
     private CharacterController characterController;
     private GameObject head;
 
-    public float speed;
-    public float gravityMultiplier;
+    public float speed = 1;
+    public float gravityMultiplier = 1;
 
     [SerializeField] [Tooltip("The deadzone that the controller movement will have to be above for instantaneous movement.")]
-    float inputDeadZone = 0.75f;
+    private float instantMoveInputDeadZone = 0.85f;
+    [SerializeField] [Tooltip("The deadzone that the controller movement will have to be above for smoothMovement")]
+    private float smoothMoveInputDeadZone = 0.15f;
 
-    public bool instantMove;
+    [SerializeField] private LayerMask layerMask = 1;
 
+    [SerializeField] private bool instantMove;
     [SerializeField] private bool moveOnlyOnTeleportArea;
 
-    private enum InputAxes
+    public enum InputAxes
     {
         Primary2DAxis = 0,
         Secondary2DAxis = 1,
@@ -43,87 +45,50 @@ public class PlayerRigMovement : MonoBehaviour
     private void FixedUpdate()
     {
         PositionController();
-        CheckForInput();
+
+        if (instantMove)
+            CheckForInputInstantMovement();
+        else
+            CheckForInput();
+
         ApplyGravity();
     }
 
 
     private bool isMoving;
 
+
     private void CheckForInputInstantMovement()
     {
-//        foreach (var controller in controllers)
-//        {
-//            if (controller.enableInputActions)
-//                CheckForMovement(controller.inputDevice);
-//        }
+        InputDevices.GetDeviceAtXRNode(controllerType).TryGetFeatureValue(InputAxesToCommonUsage[(int) TurningInput], out Vector2 inputAxis);
 
-        foreach (var node in xrNodes)
+        if (Mathf.Abs(inputAxis.x) > instantMoveInputDeadZone || Mathf.Abs(inputAxis.y) > instantMoveInputDeadZone)
         {
-            InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(InputAxesToCommonUsage[(int) TurningInput], out Vector2 inputAxis);
-
-            //Test stop from both controllers trying to move player at same time
-            if (inputAxis != Vector2.zero && !isMoving)
-            {
-                if (Mathf.Abs(inputAxis.x) > inputDeadZone || Mathf.Abs(inputAxis.y) > inputDeadZone)
-                {
-                    StartMove(inputAxis);
-                    isMoving = true;
-                }
-
-                return;
-            }
-
-            isMoving = false;
+            if (!isMoving)
+                StartMove(inputAxis);
+            isMoving = true;
+            return;
         }
+
+        isMoving = false;
     }
 
     private void CheckForInput()
     {
-//        foreach (var controller in controllers)
-//        {
-//            if (controller.enableInputActions)
-//                CheckForMovement(controller.inputDevice);
-//        }
-
-        foreach (var node in xrNodes)
-        {
-            InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(InputAxesToCommonUsage[(int) TurningInput], out Vector2 inputAxis);
-            StartMove(inputAxis);
-
-            //Test stop from both controllers trying to move player at same time
-            if (inputAxis != Vector2.zero)
-            {
-                return;
-            }
-        }
+        InputDevices.GetDeviceAtXRNode(controllerType).TryGetFeatureValue(InputAxesToCommonUsage[(int) TurningInput], out Vector2 inputAxis);
+        if (Mathf.Abs(inputAxis.x) > smoothMoveInputDeadZone && !(Mathf.Abs(inputAxis.y) > smoothMoveInputDeadZone))
+        StartMove(inputAxis);
     }
-
-//    private void CheckForMovement(InputDevice controllerInputDevice)
-//    {
-//        if (controllerInputDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 position))
-//        {
-//            StartMove(position);
-//        }
-//    }
 
     private void StartMove(Vector2 position)
     {
-//        Vector3 direction = new Vector3(position.x, 0, position.y);
-//        Vector3 rotation = new Vector3(0, head.transform.eulerAngles.y, 0);
-//
-//        direction = Quaternion.Euler(rotation) * direction;
-//
-//        Vector3 movement = direction * speed;
-
         Quaternion headDirection = Quaternion.Euler(0, head.transform.eulerAngles.y, 0);
         Vector3 moveDirection = headDirection * new Vector3(position.x, 0, position.y);
 
-
         if (instantMove && CheckIfTeleportationGround(moveDirection * speed))
             characterController.Move(moveDirection * speed);
-        else if (CheckIfTeleportationGround(moveDirection * speed))
-            characterController.Move(moveDirection * Time.deltaTime * speed);
+        else if (CheckIfTeleportationGround(moveDirection * (Time.deltaTime * speed)))
+            characterController.Move(moveDirection * (Time.deltaTime * speed));
     }
 
     private void PositionController()
@@ -139,34 +104,29 @@ public class PlayerRigMovement : MonoBehaviour
         characterController.center = newCenter;
     }
 
-    private float fallingSpeed;
-    private int layerMask;
+    private float fallSpeed;
 
     private void ApplyGravity()
     {
-//        Vector3 gravity = gravityMulti * Time.deltaTime * new Vector3(0, Physics.gravity.y, 0);
-//        characterController.Move(gravity * Time.deltaTime);
+        if (characterController.isGrounded) fallSpeed = 0;
+        else fallSpeed += Physics.gravity.y * Time.fixedDeltaTime;
 
-        if (characterController.isGrounded)
-            fallingSpeed = 0;
-        else
-            fallingSpeed += Physics.gravity.y * Time.fixedDeltaTime;
-
-        characterController.Move(fallingSpeed * Time.fixedDeltaTime * Vector3.up * gravityMultiplier);
+        characterController.Move(fallSpeed * Time.fixedDeltaTime * Vector3.up * gravityMultiplier);
     }
 
-    private bool isGrounded()
-    {
-        Vector3 startPos = transform.TransformPoint(characterController.center);
-        return Physics.SphereCast(startPos, characterController.radius, Vector3.down, out RaycastHit hit, characterController.center.y + .01f, layerMask);
-    }
+    // private bool isGrounded()
+    // {
+    //     Vector3 startPos = transform.TransformPoint(characterController.center);
+    //     return Physics.SphereCast(startPos, characterController.radius, Vector3.down, out RaycastHit hit, characterController.center.y + .01f, layerMask);
+    // }
 
     private bool CheckIfTeleportationGround(Vector3 movedirection)
     {
         if (!moveOnlyOnTeleportArea) return true;
-        
+
         Vector3 startPos = transform.TransformPoint(characterController.center) + movedirection;
-        Physics.Raycast(startPos, Vector3.down, out RaycastHit hit, characterController.center.y + .1f, layerMask);
-        return hit.collider.GetComponent<TeleportationArea>();
+        Debug.DrawRay(startPos, Vector3.down * (characterController.center.y + .1f), Color.yellow);
+        return Physics.Raycast(startPos, Vector3.down, out RaycastHit hit, characterController.center.y + 1, layerMask) 
+            ? hit.collider.attachedRigidbody?.GetComponent<TeleportationArea>() : false;
     }
 }
