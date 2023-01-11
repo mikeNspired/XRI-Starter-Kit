1c8b9af34b945e1753bc536ee4b56cf03f35d470
Mikenspired <Mikenspired@gmail.com>
2023-01-10 16:54:54 -0800

    Add XRI knobs and levers

--- Assets/DebugLog.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DebugLog : MonoBehaviour
-{
-    public void LogMessage(string message) => Debug.Log(message);
-}
