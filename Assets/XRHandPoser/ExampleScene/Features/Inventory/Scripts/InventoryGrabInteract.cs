c7a580bcdbee1ae836e62e8b4f870f22e89daaa3
Mikenspired <Mikenspired@gmail.com>
2023-01-19 17:38:18 -0800

    Simplify inventory grab interact

--- Assets/XRHandPoser/ExampleScene/Features/Inventory/Scripts/InventoryGrabInteract.cs
+++ Assets/XRHandPoser/ExampleScene/Features/Inventory/Scripts/InventoryGrabInteract.cs
@@ -8,9 +8,7 @@ namespace MikeNspired.UnityXRHandPoser
     {
         [SerializeField] private InteractButton interactButton = InteractButton.grip;
         [SerializeField] private InventoryManager inventoryManager;
-        [SerializeField] private bool addToSlotOnRelease = true;
 
-        private bool isLeftGripped, isRightGripped;
         private List<ActionBasedController> controllers = new List<ActionBasedController>();
         private InventorySlot inventorySlot;
         private ActionBasedController leftHand, rightHand;
@@ -54,45 +52,10 @@ namespace MikeNspired.UnityXRHandPoser
                 rightHand = inventoryManager.rightController;
         }
 
-        private void Update()
-        {
-            if (controllers.Count == 0) return;
-            for (var index = 0; index < controllers.Count; index++) TryInteractIfGripping(controllers[index]);
-        }
-
-        private void TryInteractIfGripping(ActionBasedController controller)
-        {
-            if (isLeftGripped && controller == leftHand)
-            {
-                inventorySlot.TryInteractWithSlot(controller.GetComponentInChildren<XRDirectInteractor>());
-                isLeftGripped = false;
-            }
-            else if (isRightGripped && controller == rightHand)
-            {
-                Debug.Log("Interacting: " + isRightGripped);
-                inventorySlot.TryInteractWithSlot(controller.GetComponentInChildren<XRDirectInteractor>());
-                isRightGripped = false;
-            }
-        }
-
         private void SetControllerGrip(ActionBasedController controller, bool state)
         {
-                            Debug.Log("Set Controller Grip: " + controller.name + " " +  state);
-
-            if (controller == leftHand)
-                isLeftGripped = state;
-            else
-                isRightGripped = state;
-
-            TryInteractOnRelease(controller);
-        }
-
-        private void TryInteractOnRelease(ActionBasedController controller)
-        {
-            if (!addToSlotOnRelease) return;
-            var hasSelection = controller.GetComponentInChildren<XRDirectInteractor>().hasSelection;
-            if (hasSelection && controllers.Contains(controller))
-                inventorySlot.TryInteractWithSlot(controller.GetComponentInChildren<XRDirectInteractor>());
+            if (!controllers.Contains(controller)) return;
+            inventorySlot.TryInteractWithSlot(controller.GetComponentInChildren<XRDirectInteractor>());
         }
 
         private void OnTriggerEnter(Collider other)
@@ -100,30 +63,7 @@ namespace MikeNspired.UnityXRHandPoser
             if (!other.TryGetComponent(out XRBaseInteractor interactor)) return;
             var controller = interactor.GetComponentInParent<ActionBasedController>();
 
-            if (controller && !controllers.Contains(controller))
-            {
-                controllers.Add(controller);
-
-                // isGripped is marked as false when a slot is interacted with to prevent repeated interaction.
-                // In that event, this checks that again if the player moved the hand in and out of the slot
-                var gripState = GetTriggerOrGripValue();
-                
-                if (controller == leftHand)
-                    isLeftGripped = gripState;
-                else
-                    isRightGripped = gripState;
-            }
-            
-            bool GetTriggerOrGripValue()
-            {
-                var gripState = false;
-
-                if (interactButton == InteractButton.grip)
-                    gripState = (int)controller.selectActionValue.action.ReadValue<float>() == 1;
-                else
-                    gripState = (int)controller.activateActionValue.action.ReadValue<float>() == 1;
-                return gripState;
-            }
+            if (controller && !controllers.Contains(controller)) controllers.Add(controller);
         }
 
         private void OnTriggerExit(Collider other)
